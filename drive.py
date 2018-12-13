bba0b91ce20678fd414267a632e41f96bdd0afa1
ponylama <intelg@gmail.com>
2018-12-13 12:24:34 +0200

    send number of nodes and edges\degrees

--- drive.py
+++ drive.py
@@ -1,29 +1,39 @@
-from tkinter import *
+# from gui import *
+
 from networkx import *
 import matplotlib.pyplot as plt
 
 
 class Drive:
+    v = 10
+    e = 10
 
     def __init__(self):
         print("init")
 
-    @staticmethod
-    def regular_graph():
-        draw(random_regular_graph(4, 10))
+    def regular_graph(self):
+        draw(random_regular_graph(self.e, self.v))
         plt.show()
 
-    @staticmethod
-    def tree_graph():
-        draw(random_tree(10))
+    def tree_graph(self):
+        draw(random_tree(self.v))
         plt.show()
 
-    @staticmethod
-    def random_graph():
-        draw(gnm_random_graph(10, 20))
+    def random_graph(self):
+        draw(gnm_random_graph(self.v, self.e))
         plt.show()
 
+    def update_v(self, v_input):
+        self.v = v_input
 
+    def update_e(self, e_input):
+        self.e = e_input
+
+
+# a = Drive()
+# print(a.v)
+# a.update_v(14)
+# print(a.v)
 
 
 
--- gui.py
+++ gui.py
@@ -1,72 +1,75 @@
 from drive import *
+from tkinter import *
 
 
-# class Gui:
-#     D = Drive()
+class GUI:
+    def __init__(self, root, D):
+        self.root = root
+        root.title("A simple GUI")
 
-#     label1 = Label(top, text="Choose which Graph you would like:")
-#     randomButton = Button(frame, text="Random Graph", command=D.randomGraph())
-#     regularButton = Button(frame, text="Regular Graph", command=D.regularGraph())
-#     treeButton = Button(frame, text="Tree Graph", command=D.treeGraph)
-#
-#     label1.pack()
-#     randomButton.pack(side=LEFT)
-#     regularButton.pack(side=LEFT)
-#     treeButton.pack(side=LEFT)
-#     frame.pack()
-#     top.mainloop()
-#
-#
-# g = Gui()
-
-# from tkinter import Tk, Label, Button
-# D = Drive()
-#
-#
-# class MyFirstGUI:
-#     def __init__(self, master):
-#         self.master = master
-#         master.title("A simple GUI")
-#         frame = Frame(master)
-#         self.label1 = Label(master, text="Choose which Graph you would like:")
-#         self.randomButton = Button(frame, text="Random Graph", command=D.randomGraph(self))
-#         self.regularButton = Button(frame, text="Regular Graph", command=D.regularGraph())
-#         self.treeButton = Button(frame, text="Tree Graph", command=D.treeGraph)
-#         self.label1.pack()
-#         self.randomButton.pack(side=LEFT)
-#         self.regularButton.pack(side=LEFT)
-#         self.treeButton.pack(side=LEFT)
-#         frame.pack()
-#
-#
-# root = Tk()
-# my_gui = MyFirstGUI(root)
-# root.mainloop()
-
-from tkinter import Tk, Label, Button
-
-
-
-class MyFirstGUI:
-    def __init__(self, master, D):
-        self.master = master
-        master.title("A simple GUI")
-
-        self.label = Label(master, text="Choose which Graph you would like:")
+        self.label = Label(root, text="Choose which Graph you would like:")
         self.label.pack()
 
-        self.randomButton = Button(master, text="Random Graph", command=D.random_graph)
+        self.randomButton = Button(root, text="Random Graph", command=self.build_random)
         self.randomButton.pack(side=LEFT)
 
-        self.regularButton = Button(master, text="Regular Graph", command=D.regular_graph)
+        self.regularButton = Button(root, text="Regular Graph", command=self.build_regular)
         self.regularButton.pack(side=LEFT)
 
-        self.treeButton = Button(master, text="Tree Graph", command=D.tree_graph)
+        self.treeButton = Button(root, text="Tree Graph", command=self.build_tree)
         self.treeButton.pack(side=LEFT)
+
+        self.textBox_v = Text(root, height=2, width=10)
+        self.textBox_v.pack(side=LEFT)
+        self.textBox_v.insert('1.0', "vertics", "vertics")
+
+        self.textBox_e = Text(root, height=2, width=15)
+        self.textBox_e.pack(side=LEFT)
+        self.textBox_e.insert('1.0', "edges \ degree", "edges")
+
+        self.textBox_msg = Text(root, height=2, width=22)
+        self.textBox_msg.pack(side=LEFT)
+        self.textBox_msg.insert('1.0', "msg to user if needed", "edges")
+
+    def p(self):
+            print(self.textBox.get(1))
+
+    def build_tree(self):
+        D.update_v(int(self.textBox_v.get("1.0", "end-1c")))
+        D.update_e(int(self.textBox_e.get("1.0", "end-1c")))
+        D.tree_graph()
+
+    def build_regular(self):
+        D.update_v(int(self.textBox_v.get("1.0", "end-1c")))
+        D.update_e(int(self.textBox_e.get("1.0", "end-1c")))
+        if D.e * D.v % 2 == 0 and D.v > D.e:
+            D.regular_graph()
+        elif D.e * D.v % 2 != 0:
+            print("v * e must be even")
+            self.textBox_msg.delete('1.0', END)
+            self.textBox_msg.insert('1.0', "in regular Graph v * d must be even", "edges" )
+        elif D.v <= D.e:
+            print("should be more degrees than nodes")
+            self.textBox_msg.delete('1.0', END)
+            self.textBox_msg.insert('1.0', "should be more degrees than nodes", "edges")
+
+    def build_random(self):
+        D.update_v(int(self.textBox_v.get("1.0", "end-1c")))
+        D.update_e(int(self.textBox_e.get("1.0", "end-1c")))
+        D.random_graph()
+
+
+
+
+
+    # buttonCommit = Button(root, height=1, width=10, text="Commit",
+    #                       command=lambda: retrieve_input(self))
+    # command=lambda: retrieve_input() >>> just means do this when i press the button
+    # buttonCommit.pack()
 
 
 root = Tk()
 D = Drive()
-my_gui = MyFirstGUI(root, D)
+my_gui = GUI(root, D)
 root.mainloop()
 
