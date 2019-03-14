c62a32d6b65c0351462e9409347489ccb554bcea
ponylama <intelg@gmail.com>
2019-03-14 10:27:26 +0200

    add colors to graph

--- Graph_from_txt.py
+++ Graph_from_txt.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from networkx import *
+import drive
 
 class GraphFromTxt:
     def __init__(self, text): # init from text file
@@ -28,6 +29,25 @@ G = G_listed.parse()
 
 # draw(G)
 # plt.show()
+
+node_colors = []
+for i in range(0,8):
+        if i < 2:
+           node_colors.append('b')
+        else:
+           node_colors.append('r')
+"""
+add colors to grapg
+https://stackoverflow.com/questions/20523327/python-and-networkx-how-to-change-the-color-of-nodes
+"""
+node_colors[4] = 'g'
+print(node_colors)
+nx.draw_circular(G, node_color=node_colors)
+plt.show()
+
+p = drive()
+
+
 
 """
 add field of stepped node to each node in graph.
