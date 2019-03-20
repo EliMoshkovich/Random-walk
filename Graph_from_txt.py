2da9429f8e0bc49783e0ab67c534d09fbacfb883
ponylama <intelg@gmail.com>
2019-03-20 18:32:47 +0200

    min_stepped is working

--- Graph_from_txt.py
+++ Graph_from_txt.py
@@ -16,7 +16,7 @@ class GraphFromTxt:
         print(self.GraphStan)
 
     def length(self):
-        print(self.GraphStan.__len__())
+        print("num of nodes is: " + str(self.GraphStan.__len__()))
 
     def print_edges(self, G):
         print(G.edges())
@@ -40,36 +40,79 @@ add colors to grapg
 https://stackoverflow.com/questions/20523327/python-and-networkx-how-to-change-the-color-of-nodes
 """
 
-print(node_colors)
-nx.draw_circular(G, node_color=node_colors)
-plt.show()
-plt.savefig('visual graphs\\books_read2.png')
+# print(node_colors)
+# nx.draw_circular(G, node_color=node_colors)
+# plt.show()
+# plt.savefig('visual graphs\\books_read2.png')
 
 """
 add field of stepped node to each node in graph.
 https://networkx.github.io/documentation/networkx-1.9/reference/generated/networkx.classes.function.set_node_attributes.html
 """
+
 nx.set_node_attributes(G, 0 , 'step')
 print(sorted(G.nodes))
-print(G.node[2]['step'])
-G.node[2]['step'] += 1
-print(G.node[2]['step'])
+# print(G.node[2]['step'])
+G.node[2]['step'] += 3
+G.node[3]['step'] += 2
+G.node[4]['step'] += 1
+# print(G.node[2]['step'])
 
-tree = nx.bfs_tree(G,1)
-print(tree)
+"""
+tree test 
+"""
+# tree = nx.bfs_tree(G, 1)
+# tree = dict(nx.bfs_predecessors(G, 1))
+# print(tree)
+# draw(tree)
+# plt.show()
+
+
+def stepped(G, node_index):
+    # print("node " + str(node_index) + " has stepped " + str(G.node[node_index]['step']) + " times")
+    return G.node[node_index]['step']
+
+
+"""
+param: list of nodes num
+return : node with lowest stepped times 
+"""
+
+
+def min_stepped(G, adj):
+    if len(adj) > 0:
+        min_step = 0
+        node_index = 0
+        for node in adj:  # init min_step
+            min_step = stepped(G, node[0])
+            node_index = node[0]
+            break
+
+        for node in adj:
+            n_stepped = stepped(G, node[0])
+            if(n_stepped < min_step):
+                min_step = n_stepped
+                node_index = node[0]
+    return node_index
+
+
+"""
+random walk algoritm
+"""
+
 
 def random_walk(G):
+    print("random walk function")
     nodes = sorted(G.nodes())
     s = nodes[0]
-    # print(G.adj[s])
-    for nbr, datadict in G.adj[s].items():
-        print(nbr)
-
-    adj = G.adj[s].items()
+    adj_n = G.adj[s].items()
+    # print(adj_n)
+    print(min_stepped(G, adj_n))
 
 
-draw(tree)
-plt.show()
+
+random_walk(G)
+
 
 # random_walk(G)
 
